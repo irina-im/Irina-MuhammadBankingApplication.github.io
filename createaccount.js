6722d9df722ff0675d30b4ab0f7df356bb0aa441
Irina Muhammad <87255195+irina-im@users.noreply.github.com>
2022-01-19 23:45:11 -0500

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -50,7 +50,9 @@ function CreateAccount(){
               Name<br/>
               <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
               Email address<br/>
-              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => {
+                validate(e.currentTarget.value);
+                setEmail(e.currentTarget.value);}}/><br/>
               Password (min 8 characters)<br/>
               <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
               <button type="submit" className="btn btn-light" onClick={handleCreate} disabled={!name && !email && !password}>Create Account</button>
